7741ba8645bc297247017d267011090ab85a1ebd
jabit0g23 <jabit0g23@gmail.com>
2024-11-04 16:19:20 -0400

    CRUD en memoria

--- src/main/java/com/example/customers/controllers/CustomerController.java
+++ src/main/java/com/example/customers/controllers/CustomerController.java
@@ -1,7 +1,7 @@
 package com.example.customers.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.customers.entities.Customers;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,28 +12,93 @@ import java.util.Map;
 
 public class CustomerController {
 
-    @GetMapping("/prueba")
-    public String prueba(){
-        return "hola mundo";
+    private  List<Customers> listC = new ArrayList<>();
+
+    public CustomerController(){
+
+        Customers c = new Customers();
+        c.setId(1);
+        c.setName("Javier");
+        c.setLastname("Gonzalez");
+        c.setAddress("asdasdasd");
+        c.setEmail("asdasdasd");
+        listC.add(c);
+
+        Customers c2 = new Customers();
+        c2.setId(2);
+        c2.setName("Javier");
+        c2.setLastname("Gonzalez");
+        c2.setAddress("asdasdasd");
+        c2.setEmail("asdasdasd");
+        listC.add(c2);
+    };
+
+    @GetMapping("/customer/{id}") //traer un cliente
+    public Customers getCustomer(@PathVariable Integer id){
+        for (Customers customer : listC){
+            if (customer.getId() == id){
+                return  customer;
+            }
+        }
+        return null;
     }
 
-    @GetMapping("/prueba2")
-    public List<String> prueba2(){
-        List<String> list = new ArrayList<>();
-        list.add("javier");
-        list.add("javier2");
-        list.add("javier3");
-        return list;
+    @GetMapping("/customer") //traer todos los clientes
+    public List<Customers> getAllCustomers(){
+        return listC;
     }
 
-    @GetMapping("/prueba3")
-    public Map<String, String> prueba3(){
-        Map<String, String> map = new HashMap<>();
-        map.put("clave1" , "valor1");
-        map.put("clave2" , "valor2");
-        map.put("clave3" , "valor3");
-        map.put("clave4" , "valor4");
-        map.put("clave5" , "valor5");
-        return map;
+    @DeleteMapping("/customer/{id}") //Eliminar cliente
+    public void removeCustomer(@PathVariable Integer id){
+        for (Customers customer : listC) {
+            if (customer.getId() == id) {
+                listC.remove(customer);
+                break;
+            }
+        }
+    }
+
+    @PostMapping("/customer") //Agregar cliente
+    public void addCustomer(@RequestBody Customers customer){
+        listC.add(customer);
+    }
+
+    @PutMapping("/customer/{id}") //Modificar cliente
+    public void updateCustomer(@PathVariable Integer id, @RequestBody Customers updateCustomer){
+        for (Customers customer : listC) {
+            if (customer.getId() == id) {
+                listC.remove(customer);
+                updateCustomer.setId(id);
+                listC.add(updateCustomer);
+                break;
+            }
+        }
+
+    }
+
+    //http://localhost:8080/customer/search/?name=javier
+    @GetMapping("/customer/search") //Buscar cliente
+    public List<Customers> searchCustomer(@RequestParam(name = "email" , required = false) String email,
+                                          @RequestParam(name = "name", required = false) String name){
+
+        List<Customers> searchResults = new ArrayList<>();
+
+        if (email != null){
+            for (Customers customer : listC) {
+                if (customer.getEmail().contains(email)) {
+                    searchResults.add(customer);
+                }
+            }
+        }
+
+        if (name != null) {
+            for (Customers customer : listC) {
+                if (customer.getName().contains(name)) {
+                    searchResults.add(customer);
+                }
+            }
+        }
+
+        return searchResults;
     }
 }
--- src/main/java/com/example/customers/entities/Customers.java
+++ src/main/java/com/example/customers/entities/Customers.java
@@ -1,73 +1,16 @@
 package com.example.customers.entities;
 
+import lombok.*;
+
 import java.util.Objects;
 
+@Data
 public class Customers {
 
+    private Integer id;
     private String name;
     private String lastname;
     private String email;
     private String address;
 
-    public Customers(String name, String lastname, String email, String address) {
-        this.name = name;
-        this.lastname = lastname;
-        this.email = email;
-        this.address = address;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Customers customers = (Customers) o;
-        return Objects.equals(name, customers.name) && Objects.equals(lastname, customers.lastname) && Objects.equals(email, customers.email) && Objects.equals(address, customers.address);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, lastname, email, address);
-    }
-
-    @Override
-    public String toString() {
-        return "Customers{" +
-                "name='" + name + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
